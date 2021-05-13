93fe2458ac8fd39df8f4cf6bb87ab88962e78f63
Daniel Railean <daniel.railean1@gmail.com>
2021-05-13 13:06:48 +0200

    Changes to business tier
    Added Ingredient service blank and blank controllers

---
+++ Business logic tier/API/Data/IIngredioentService.cs
@@ -0,0 +1,12 @@
+﻿using API.Models;
+
+namespace API.Data
+{
+    public interface IIngredientService
+    {
+        Ingredient AddIngredient(Ingredient ingredient);
+        Ingredient GetIngredient(int id);
+        Ingredient UpdateIngredient(Ingredient ingredient, string password);
+        Ingredient RemoveIngredient(Ingredient ingredient);
+    }
+}
---
+++ Business logic tier/API/Data/IngredientServiceJSON.cs
@@ -0,0 +1,27 @@
+﻿using API.Models;
+
+namespace API.Data
+{
+    public class IngredientServiceJSON : IIngredientService 
+    {
+        public Ingredient AddIngredient(Ingredient ingredient)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Ingredient GetIngredient(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Ingredient UpdateIngredient(Ingredient ingredient, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Ingredient RemoveIngredient(Ingredient ingredient)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
