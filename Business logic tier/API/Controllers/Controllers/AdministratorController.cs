20edded148cc423160fb8007996ffea9e26abed4
dimitrian <dimitrian.cebotaru@gmail.com>
2021-05-13 18:34:28 +0200

    AdminController and its interface

---
+++ Business logic tier/API/Controllers/Controllers/AdministratorController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class AdministratorController : ControllerBase, IAdministratorController
+    {
+        private IAdminService adminService;
+
+        public AdministratorController(IAdminService adminService)
+        {
+            this.adminService = adminService;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<Administrator>> ValidateAdministrator(string email, string password)
+        {
+            try
+            {
+                Administrator valid = adminService.ValidateAdministrator(email,password);
+                return Ok(valid);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
--- Business logic tier/API/Controllers/IAdministratorController.cs
+++ Business logic tier/API/Controllers/IAdministratorController.cs
@@ -1,7 +1,12 @@
-﻿namespace API.Data
+﻿using System.Threading.Tasks;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Data
 {
     public interface IAdministratorController
     {
-        
+        Task<ActionResult<Administrator>> ValidateAdministrator([FromBody] string email, string password);
+
     }
 }
