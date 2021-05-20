e8c2848d9f0cede7c9ca92e6e0520f4ca05af3d2
Satish627 <Satish.grg627@gmail.com>
2021-05-20 20:46:08 +0200

    service and repo for recipe.

--- Data tier/Java/src/main/java/via/sep3/food/Repository/RecipeRepository.java
+++ Data tier/Java/src/main/java/via/sep3/food/Repository/RecipeRepository.java
@@ -4,5 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import via.sep3.food.Model.Recipe;
 import via.sep3.food.Model.User;
 
+import java.util.List;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
+
+    List<Recipe> findAllRecipes();
+
+    Recipe findRecipeByName(String name);
+
+    Recipe deleteRecipe(Recipe recipe);
 }
--- Data tier/Java/src/main/java/via/sep3/food/Service/ServiceImpl/RecipeService.java
+++ Data tier/Java/src/main/java/via/sep3/food/Service/ServiceImpl/RecipeService.java
@@ -1,4 +1,42 @@
 package via.sep3.food.Service.ServiceImpl;
 
-public class RecipeService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import via.sep3.food.Model.Recipe;
+import via.sep3.food.Repository.RecipeRepository;
+import via.sep3.food.Service.IRecipeService;
+
+import java.util.List;
+
+@Service
+public class RecipeService implements IRecipeService {
+
+  @Autowired
+  public RecipeRepository recipeRepository;
+
+
+    @Override
+    public List<Recipe> GetAllRecipes() {
+        return recipeRepository.findAllRecipes();
+    }
+
+    @Override
+    public Recipe GetRecipe(String Name) {
+        return recipeRepository.findRecipeByName(Name);
+    }
+
+    @Override
+    public Recipe AddRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
+
+    @Override
+    public Recipe RemoveRecipe(Recipe recipe) {
+        return recipeRepository.deleteRecipe(recipe);
+    }
+
+    @Override
+    public Recipe UpdateRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
 }
