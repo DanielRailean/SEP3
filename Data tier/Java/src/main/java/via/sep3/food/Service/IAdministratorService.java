47eebdafb855231d508b8fe43dd6d5da9761a9e0
Daniel Railean <daniel.railean1@gmail.com>
2021-05-23 00:00:47 +0200

    Removed Administrator related stuff
    Removed each class that has connection with Administrator

--- Data tier/Java/src/main/java/via/sep3/food/Service/IAdministratorService.java
+++
@@ -1,7 +0,0 @@
-package via.sep3.food.Service;
-
-import via.sep3.food.Model.Administrator;
-
-public interface IAdministratorService {
-    Administrator ValidateAdministrator(String Email, String Password) throws Exception;
-}
