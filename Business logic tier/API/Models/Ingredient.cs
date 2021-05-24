d508a9ed41ae7a49fa1b753a1fa2b78752132aed
Daniel Railean <daniel.railean1@gmail.com>
2021-05-24 14:20:57 +0200

    Ingredient BusinessTier Refactoring
    Ingredient CRUD operations are now working in the business tier.

--- Business logic tier/API/Models/Ingredient.cs
+++ Business logic tier/API/Models/Ingredient.cs
@@ -1,11 +1,13 @@
-﻿namespace API.Models
+﻿using System.Text.Json.Serialization;
+
+namespace API.Models
 {
     public class Ingredient
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string UnitOfMeasure { get; set; }
-        public int Quantity { get; set; }
         public double Calories { get; set; }
     }
 }
