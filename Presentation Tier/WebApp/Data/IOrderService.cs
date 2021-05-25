5b945b86e25a5c31f3d54bb99db86bf876449601
Daniel Railean <daniel.railean1@gmail.com>
2021-05-25 11:34:35 +0200

    Presentation services refactoring.
    Change Presentation Interfaces to fit Business tier endpoints.

--- Presentation Tier/WebApp/Data/IOrderService.cs
+++ Presentation Tier/WebApp/Data/IOrderService.cs
@@ -6,9 +6,8 @@ namespace WebApp.Data
 {
     public interface IOrderService
     {
-        // Task<IList<Order>> GetAllOrdersAsync();
         Task<Order> GetOrderAsync(int orderId);
-        Task<IList<Order>> GetOrdersByUserAsync(User user);
+        Task<IList<Order>> GetOrdersByUserAsync(string email, string password);
         Task CreateOrderAsync(Order order);
         Task RemoveOrderAsync(Order order);
         Task UpdateOrderAsync(Order order);
