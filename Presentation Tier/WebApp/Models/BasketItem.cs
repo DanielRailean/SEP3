ca0a413b2b87b5a5fc6649f67aef55c59d037316
Daniel Railean <daniel.railean1@gmail.com>
2021-05-25 13:35:07 +0200

    Adding to basket now works
    1.Fixed basked adding functionality on index page.
    2.Changed Recipe model.

--- Presentation Tier/WebApp/Models/BasketItem.cs
+++ Presentation Tier/WebApp/Models/BasketItem.cs
@@ -4,7 +4,14 @@ namespace WebApp.Models
 {
     public class BasketItem
     {
+        public BasketItem(int amount, int recipeId)
+        {
+            Amount = amount;
+            RecipeId = recipeId;
+        }
+
         public int Amount { get; set; }
         public int RecipeId { get; set; }
+        
     }
 }
