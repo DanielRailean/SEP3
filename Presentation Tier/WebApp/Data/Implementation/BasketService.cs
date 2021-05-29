14993cf03e2ae5442d21aa937b1e9cced1d429f4
markvinczehu <55513132+markvinczehu@users.noreply.github.com>
2021-05-29 10:43:49 +0200

    Basket and Ingredient Service documentation

--- Presentation Tier/WebApp/Data/Implementation/BasketService.cs
+++ Presentation Tier/WebApp/Data/Implementation/BasketService.cs
@@ -23,16 +23,31 @@ namespace WebApp.Data.Implementation
             Fetch();
         }
 
+        /// <summary>
+        /// This method gets all the elements from the storage.
+        /// </summary>
+        /// <returns>A list of BasketItems.</returns>
         public async Task<IList<BasketItem>> GetAllBasketItems()
         {
             return basketItems;
         }
 
+        /// <summary>
+        /// This method gets all the elements from the storage.
+        /// </summary>
+        /// <returns>A list of Recipes.</returns>
         public async Task<IList<Recipe>> GetAllBasketRecipes()
         {
             return addedRecipes;
         }
 
+        /// <summary>
+        /// Matching the recipe to a BasketItem, by creating one,
+        /// then adding both into basketItems and addedRecipes list,
+        /// then call the Save() to write to LocalStorage.
+        /// </summary>
+        /// <param name="recipe">Recipe item to be added.</param>
+        /// <exception cref="Exception">The amount of added item has to be more than 0 and maximum 10.</exception>
         public async Task AddRecipe(Recipe recipe)
         {
             BasketItem item = new BasketItem(recipe.Amount, recipe.Id);
@@ -56,6 +71,13 @@ namespace WebApp.Data.Implementation
             await Save();
         }
 
+        /// <summary>
+        /// This method calls the Fetch() to get
+        /// all the items from LocalStorage, then if
+        /// the parameter matches an item in the two list,
+        /// it removes it, then calls Save() to write to LocalStorage.
+        /// </summary>
+        /// <param name="recipe">Recipe item to remove.</param>
         public async Task RemoveRecipe(Recipe recipe)
         {
             await Fetch();
@@ -67,6 +89,9 @@ namespace WebApp.Data.Implementation
             await Save();
         }
 
+        /// <summary>
+        /// Clears all items from LocalStorage.
+        /// </summary>
         public async Task Clear()
         {
             addedRecipes = new List<Recipe>();
@@ -74,15 +99,29 @@ namespace WebApp.Data.Implementation
             await Save();
         }
 
+        /// <summary>
+        /// This method is to check if a list has a specific item.
+        /// </summary>
+        /// <param name="item">Item to check.</param>
+        /// <returns>A boolean value corresponding to the parameter.</returns>
         private bool ContainsRecipe(Recipe item)
         {
             return (addedRecipes.FirstOrDefault(i => i.Id == item.Id) != null);
         }
+        
+        /// <summary>
+        /// This method is to check if a list has a specific item.
+        /// </summary>
+        /// <param name="item">Item to check.</param>
+        /// <returns>A boolean value corresponding to the parameter.</returns>
         private bool ContainsItem(BasketItem item)
         {
             return (basketItems.FirstOrDefault(i => i.RecipeId == item.RecipeId) != null);
         }
 
+        /// <summary>
+        /// Writes the two lists to the LocalStorage.
+        /// </summary>
         private async Task Save()
         {
             string serialised = JsonSerializer.Serialize(basketItems);
@@ -91,6 +130,9 @@ namespace WebApp.Data.Implementation
             await localStorage.SetItemAsync(RecipesCookieName, serialised);
         }
 
+        /// <summary>
+        /// Fills the two lists with the values from LocalStorage.
+        /// </summary>
         private async Task Fetch()
         {
             var cookieContent = await localStorage.GetItemAsync<string>(BasketCookieName);
@@ -114,11 +156,20 @@ namespace WebApp.Data.Implementation
             }
         }
 
+        /// <summary>
+        /// Deletes a specific Recipe from list.
+        /// </summary>
+        /// <param name="id">Id of Recipe.</param>
         private void removeRecipeById(int id)
         {
             Recipe toRemove = addedRecipes.FirstOrDefault(i => i.Id == id);
             if (toRemove != null) addedRecipes.Remove(toRemove);
         }
+        
+        /// <summary>
+        /// Deletes a specific BasketItem from list.
+        /// </summary>
+        /// <param name="id">Id of BasketItem.</param>
         private void removeBasketItemById(int id)
         {
             BasketItem toRemove = basketItems.FirstOrDefault(i => i.RecipeId == id);
