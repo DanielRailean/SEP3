d29b0d4cc60551f4fbff060e2814d9921aa84e76
markvinczehu <55513132+markvinczehu@users.noreply.github.com>
2021-06-01 12:10:12 +0200

    API documentation initialization, IngredientController ✓

--- Business logic tier/API/Controllers/Controllers/IngredientController.cs
+++ Business logic tier/API/Controllers/Controllers/IngredientController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    /// <summary>
+    /// Controller for providing the endpoints for ingredient services.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
-    
     public class IngredientController : ControllerBase, IIngredientController
     {
         private IIngredientService ingredientService;
@@ -18,7 +20,12 @@ namespace API.Controllers
         {
             this.ingredientService = ingredientService;
         }
-            
+           
+        /// <summary>
+        /// For adding a new ingredient.
+        /// </summary>
+        /// <param name="ingredient">Ingredient item to update, in JSON.</param>
+        /// <returns>HTTP response.</returns>
         [HttpPost]
         public async Task<ActionResult<Ingredient>> AddIngredient(Ingredient ingredient)
         {
@@ -33,7 +40,12 @@ namespace API.Controllers
                 return StatusCode(500, e.Message);
             }
         }
-
+        
+        /// <summary>
+        /// Get a specific ingredient by its id.
+        /// </summary>
+        /// <param name="id">Id of the ingredient to get.</param>
+        /// <returns>HTTP response.</returns>
         [HttpGet("GetIngredient")]
         public async Task<ActionResult<Ingredient>> GetIngredient(int id)
         {
@@ -49,6 +61,10 @@ namespace API.Controllers
             }
         }
 
+        /// <summary>
+        /// Get all the ingredients that are stored.
+        /// </summary>
+        /// <returns>HTTP response.</returns>
         [HttpGet("GetAllIngredients")]
         public async Task<ActionResult<IList<Ingredient>>> GetAllIngredients()
         {
@@ -64,7 +80,11 @@ namespace API.Controllers
             }
         }
         
-        
+        /// <summary>
+        /// Updates a specific ingredient that is stored.
+        /// </summary>
+        /// <param name="ingredient">Ingredient item to update.</param>
+        /// <returns>HTTP response.</returns>
         [HttpPut]
         public async Task<ActionResult<Ingredient>> UpdateIngredient(Ingredient ingredient)
         {
@@ -80,6 +100,11 @@ namespace API.Controllers
             }
         }
 
+        /// <summary>
+        /// Deletes a specific ingredient that is passed.
+        /// </summary>
+        /// <param name="ingredient">Ingredient item to delete.</param>
+        /// <returns>HTTP response.</returns>
         [HttpDelete]
         public async Task<ActionResult<Ingredient>> RemoveIngredient(Ingredient ingredient)
         {
